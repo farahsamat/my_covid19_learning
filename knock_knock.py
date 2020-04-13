709f06bcd6f300c062f3e3f54ec88e7f25c49853
Farahani Samat <farahanis@gmail.com>
2020-04-13 12:22:51 +1000

    Knock knock joke

---
+++ knock_knock.py
@@ -0,0 +1,8 @@
+def tell(intro, punchline):
+    print("Knock knock")
+    print("Who's there")
+    print(intro)
+    print(intro + " who?")
+    print(punchline)
+
+tell("Kanga", "Actually, it's kangaroo! :D")
